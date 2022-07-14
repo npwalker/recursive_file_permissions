a43cf5c02f9c22e3936101c21529e8fa94cc12e4
Mark Egan-Fuller <mark.egan-fuller@york.ac.uk>
2022-07-14 13:46:42 +0100

    Expand tests to include confirmation of onlyif and command.

--- spec/defines/init_spec.rb
+++ spec/defines/init_spec.rb
@@ -16,6 +16,35 @@ describe 'recursive_file_permissions' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it 'creates the appropriate onlyif command' do
+        is_expected.to contain_exec('recursive_file_permissions:/tmp/blah').with_onlyif(
+          "find /tmp/blah \"(\" -type f '!' -perm 0644 \")\" -o \"(\" -type d '!' -perm 0744 \")\" -o \"(\" '!' -user test \")\" -o \"(\" '!' -group test \")\" | grep '.*'",
+        )
+      end
+
+      case os
+      when /aix/, /solaris/
+        it 'creates the appropriate command (AIX, Solaris)' do
+          is_expected.to contain_exec('recursive_file_permissions:/tmp/blah').with_command(
+            "find /tmp/blah '(' \"(\" -type f '!' -perm 0644 \")\" ')' -exec chmod  0644 {} \\; && find /tmp/blah '(' \"(\" -type d '!' -perm 0744 \")\" ')' -exec chmod  0744 {} \\; && find /tmp/blah '(' \"(\" '!' -user test \")\" ')' -exec chown -h  test {}  \\; && find /tmp/blah '(' \"(\" '!' -group test \")\" ')' -exec chgrp -h  test {} \\;",
+          )
+
+        end
+      when /darwin/
+        it 'creates the appropriate command (Darwin)' do
+          is_expected.to contain_exec('recursive_file_permissions:/tmp/blah').with_command(
+            "find /tmp/blah '(' \"(\" -type f '!' -perm 0644 \")\" ')' -exec chmod -v 0644 {} \\; && find /tmp/blah '(' \"(\" -type d '!' -perm 0744 \")\" ')' -exec chmod -v 0744 {} \\; && find /tmp/blah '(' \"(\" '!' -user test \")\" ')' -exec chown -h -v test {}  \\; && find /tmp/blah '(' \"(\" '!' -group test \")\" ')' -exec chgrp -h -v test {} \\;",
+          )
+        end
+
+      else
+        it 'creates the appropriate command (Default)' do
+          is_expected.to contain_exec('recursive_file_permissions:/tmp/blah').with_command(
+            "find /tmp/blah '(' \"(\" -type f '!' -perm 0644 \")\" ')' -exec chmod -c 0644 {} \\; && find /tmp/blah '(' \"(\" -type d '!' -perm 0744 \")\" ')' -exec chmod -c 0744 {} \\; && find /tmp/blah '(' \"(\" '!' -user test \")\" ')' -exec chown -h -c test {}  \\; && find /tmp/blah '(' \"(\" '!' -group test \")\" ')' -exec chgrp -h -c test {} \\;",
+          )
+        end
+      end
     end
   end
 end
